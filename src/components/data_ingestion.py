314a792554c1a143a1077e4a90b2d3411bddb227
Mubashir Aziz <cs.mubashir.a@gmail.com>
2025-08-31 13:06:31 +0500

    components are little bit updated and Docker file is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,8 @@
 import os
 import sys
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append(project_root)
+
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
